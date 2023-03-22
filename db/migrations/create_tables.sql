448c17c93a3752ee8bcf7bc2a3a208cce7ba4e72
MrBTTF <zhvladi@gmail.com>
2023-03-22 16:53:28 +0100

    , added more default users

--- db/migrations/create_tables.sql
+++ db/migrations/create_tables.sql
@@ -57,7 +57,15 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO bridge;
 
 
 INSERT INTO users (user_id, email, password, nickname, token)
-values ('6e3f9165-3daf-4fdc-9b52-12e6fdd810c1', 'test1@bridge.test', '42f749ade7f9e195bf475f37a44cafcb', 'Test User', ''),
-       ('a1980803-7eb9-477a-a860-9a652adfa30d', 'zalizniak@zalizniak', 'ac0ddf9e65d57b6a56b2453386cd5db5', 'zalizniak', '');
+values ('6e3f9165-3daf-4fdc-9b52-12e6fdd810c1', 
+        'test1@bridge.test', '42f749ade7f9e195bf475f37a44cafcb', 'User1', ''),  
+       ('a1983803-7eb9-477a-a860-9a652adfa30d', 
+       'zalizniak@zalizniak', 'ac0ddf9e65d57b6a56b2453386cd5db5', 'zalizniak', ''),
+       ('be2d3a8e-5f95-4716-a0ef-a814b89dbabc', 
+       'test2@bridge.test', '42f749ade7f9e195bf475f37a44cafcb', 'SecondUser', ''),
+       ('851e1367-610f-412f-a840-4dfe0d9db38d', 
+       'test3@bridge.test', '42f749ade7f9e195bf475f37a44cafcb', 'OkUser123', ''),
+       ('af58fe77-a6bb-4169-a960-3107d7bea057', 
+       'test4@bridge.test', '42f749ade7f9e195bf475f37a44cafcb', 'LastUser1', '');
 
 
